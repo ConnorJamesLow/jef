b9bc1bc21f245e423f952c5de7963d77ccd8651b
Connor <connorjameslow@gmail.com>
2020-06-24 08:27:00 -0700

    [fix] use Any typing instead of any fn
    I am used to TypeScript �  Added some comments as well.

--- jef.py
+++ jef.py
@@ -1,14 +1,21 @@
+from typing import Any
 import json
 from observer import Observer
 from os import path
 
 
-def sync(file_path):
+def sync(file_path: str) -> Observer:
+    """
+    Create a new Observer that live-updates a given json file.
+    """
     content = {}
     with open(file_path) as file:
         content = json.load(file)
 
-    def callback(root: Observer, name: str, value: any) -> bool:
+    # Write the current state to the loaded file when a change happens
+    def callback(root: Observer, name: str, value: Any) -> bool:
         with open(file_path, 'w+') as file:
             json.dump(root.to_dict(), file)
+
+    
     return Observer(content, callback, path.basename(file_path))
